550eae9aa0a42b6d91d087a45566b660e2d4473b
santiago-Daw2 <santiagofranciscotorricovaldivieso@iescastelar.com>
2026-01-23 19:26:10 +0100

    springboot

---
+++ SpringBoot/demo/src/main/java/com/example/demo/controllers/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.example.demo.models.Greeting;
+import org.springframework.beans.factory.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.RestController;
+
+
+
+// Rest controller para iniciar el controlador
+@RestController()
+public class GreetingController {
+
+    private static final String template = "Hello, %s! "; // %s Lo podremos sustituir por el nombre de usuario que queramos
+    private final AtomicLong counter = new AtomicLong(); // Contador que se inicializa en 0
+
+    @GetMapping()
+
+    public Greeting greeting(
+        @RequestParam(value = "name", defaultValue = "World") String name
+    ) {
+        return new Greeting(counter.incrementAndGet(), template.formatted(name));
+
+    }
+}
---
+++ SpringBoot/demo/src/main/java/com/example/demo/models/Greeting.java
@@ -0,0 +1,6 @@
+package com.example.demo.models;
+
+// Creamos la clase greeting
+public record Greeting(long id, String contenido) {
+
+}
